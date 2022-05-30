aa842bd7caf9560f609d424b6ccfa912f8d67b02
yeringim <yrkim.me@gmail.com>
2022-05-30 10:13:47 +0900

    로그인

--- Test_0/RegisterActivity.cs
+++ Test_0/RegisterActivity.cs
@@ -31,11 +31,20 @@ namespace Test_0
             userName = FindViewById<EditText>(Resource.Id.editText1);
             password = FindViewById<EditText>(Resource.Id.editText2);
             registerButton = FindViewById<Button>(Resource.Id.button1);
-            
+
+            registerButton.Click += RegisterClicked;
+
 
             // 회원가입 버튼 클릭하면 이전 화면으로 이동
-            FindViewById<Button>(Resource.Id.btnRegister).Click += (o, e) =>
-            SetContentView(Resource.Layout.activity_login);
+            //FindViewById<Button>(Resource.Id.btnRegister).Click += (o, e) =>
+            //SetContentView(Resource.Layout.activity_login);
+        }
+
+
+        // 회원가입
+        private void RegisterClicked(object sender, EventArgs e)
+        {
+            
         }
     }
 }
